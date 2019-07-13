1be00615cc47976e3a383ffdf24bb1234e103be3
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-07-13 19:13:43 -0400

    Intial commit with added dependancies

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
+import logger from "redux-logger";
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
-
+const rootElement = document.getElementById("root");
 const store = createStore(
-  rootReducer
+  rootReducer, applyMiddleware(thunk)
   /* applyMiddleware goes here */
 );
 
@@ -20,5 +22,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  + rootElement
 );
